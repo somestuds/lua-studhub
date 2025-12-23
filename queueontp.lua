1946c6b7295e00eeed0b273d2350d70d333b772f
some of the studs has joined <somestudsdev@gmail.com>
2025-12-22 19:13:10 -0500

    Update queue_on_teleport function to use HttpGet

--- queueontp.lua
+++ queueontp.lua
@@ -1,3 +1,3 @@
 print("Queue On Teleport")
 local code = readfile("queueontp.lua")
-queue_on_teleport('loadstring('..code..')()')
+queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/somestuds/lua-studhub/refs/heads/master/queueontp.lua"))')
